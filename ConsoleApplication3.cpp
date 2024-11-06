281aecf2e8f99ada77721b80b576b1a70779ccd7
DoritosLover1 <ihsandmrci10@gmail.com>
2024-11-06 16:50:34 +0300

    IDDFS, benzeri bir yapılanma yapıldı ama recursive değil itaretive olarak.

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -11,7 +11,8 @@
 enum SearchType {
     BFSTYPE = 0,
     DFSTYPE = 1,
-    DFSLTYPE = 2
+    DFSLTYPE = 2,
+    IDDFSTYPE = 3
 };
 
 /*Kinda storage for avoiding reputation*/
@@ -34,7 +35,7 @@ Output file("output.txt");
 enum SearchType type;
 std::vector<int> fringeSize;
 short int tarr[3][3] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };
-int limit = 20;
+long int limit = 2000;
 
 /*x-axis and y-axis AI directions*/
 short int dRow[] = { -1, 0, 1, 0 };
@@ -48,6 +49,37 @@ bool isValidRowAndCol(int row, int col);
 bool isCorrectSolution(short int grid[3][3]);
 void printGrid(short int grid[3][3]);
 void endOfAlgorithm(SearchType type,PuzzleHistory* current, std::vector<int>* fringeSize);
+/*
+bool DLS(PuzzleHistory& current, long int depth,long int depth_limit, std::unordered_set<std::string>& visitedSet) {
+    PuzzleHistory nextCurrent;
+    if (isCorrectSolution(current.history)) {
+        std::cout << "IT IS CORRECT" << std::endl;
+        return true;
+    }
+    if (depth >= depth_limit) return false;
+
+    visitedSet.insert(current.former());
+
+    for (int i = 0; i < 4; i++) {
+        int newRow = current.RowLoc + dRow[i];
+        int newCol = current.ColLoc + dCol[i];
+
+        if (isValidRowAndCol(newRow, newCol)) {
+            nextCurrent = current;
+
+            std::swap(nextCurrent.history[current.RowLoc][current.ColLoc],
+                nextCurrent.history[newRow][newCol]);
+
+            if (visitedSet.find(nextCurrent.former()) == visitedSet.end()) {
+                visitedSet.insert(nextCurrent.former());
+                if (DLS(nextCurrent, depth + 1, depth_limit, visitedSet))
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+*/
 
 void BFS(short int grid[3][3], short int row, short int col) {
     type = BFSTYPE;
@@ -68,7 +100,7 @@ void BFS(short int grid[3][3], short int row, short int col) {
 
         fringeSize.push_back(puzzleHistoryQueue.size());
         if (isCorrectSolution(current.history)) {
-            endOfAlgorithm(BFSTYPE, &current, &fringeSize);
+            endOfAlgorithm(type, &current, &fringeSize);
             return;
         }
 
@@ -80,9 +112,9 @@ void BFS(short int grid[3][3], short int row, short int col) {
                 PuzzleHistory nextCurrent = current;
                 std::swap(nextCurrent.history[current.RowLoc][current.ColLoc],
                     nextCurrent.history[newRow][newCol]);
+
                 nextCurrent.RowLoc = newRow;
                 nextCurrent.ColLoc = newCol;
-
                 nextCurrent.path += moveString[i];
 
                 if (visitedSet.find(nextCurrent.former()) == visitedSet.end()) {
@@ -91,16 +123,13 @@ void BFS(short int grid[3][3], short int row, short int col) {
                 }
             }
         }
-
         if (++iterationCount % printInterval == 0) {
             std::cout << "Stage: " << iterationCount << "\n";
             printGrid(current.history);
         }
-
     }
     std::cout << "No Solution Found via BFS. \n";
 }
-
 void DFS(short int grid[3][3], short int row, short int col) {
     type = DFSTYPE;
     std::stack<PuzzleHistory> puzzleHistoryStack;
@@ -120,7 +149,7 @@ void DFS(short int grid[3][3], short int row, short int col) {
 
         fringeSize.push_back(puzzleHistoryStack.size());
         if (isCorrectSolution(current.history)) {
-            endOfAlgorithm(DFSTYPE, &current, &fringeSize);
+            endOfAlgorithm(type, &current, &fringeSize);
             return;
         }
 
@@ -153,9 +182,7 @@ void DFS(short int grid[3][3], short int row, short int col) {
 
     std::cout << "No Solution Found via DFS.\n";
 }
-
-// 04.11.2024 BURASI YAPILACAK.
-void DFSL(short int grid[3][3], short int row, short int col, short int limit) {
+void DFSL(short int grid[3][3], short int row, short int col, long int limit) {
     type = DFSLTYPE;
     std::stack<PuzzleHistory> puzzleHistoryStack;
     std::unordered_set<std::string> visitedSet;
@@ -178,7 +205,7 @@ void DFSL(short int grid[3][3], short int row, short int col, short int limit) {
                 puzzleHistoryStack.pop();
                 fringeSize.push_back(puzzleHistoryStack.size());
                 if (isCorrectSolution(current.history)) {
-                    endOfAlgorithm(DFSTYPE, &current, &fringeSize);
+                    endOfAlgorithm(type, &current, &fringeSize);
                     return;
                 }
 
@@ -213,6 +240,72 @@ void DFSL(short int grid[3][3], short int row, short int col, short int limit) {
 
     std::cout << "No Solution Found via DFSL.\n";
 }
+void IDDFS(short int grid[3][3], short int row, short int col, long int depth_limit) {
+    type = IDDFSTYPE;
+    std::queue<PuzzleHistory> puzzleHistoryQueue;
+    std::unordered_set<std::string> visitedSet;
+
+    PuzzleHistory firstState = { {}, row, col, "" };
+    std::memcpy(firstState.history, grid, sizeof(firstState.history));
+    puzzleHistoryQueue.push(firstState);
+    visitedSet.insert(firstState.former());
+
+    int iterationCount = 0;
+    const int printInterval = 6000;
+    int depth = 0;
+
+    while (!puzzleHistoryQueue.empty() && depth <= depth_limit) {
+        PuzzleHistory current = puzzleHistoryQueue.front();
+        puzzleHistoryQueue.pop();
+
+        fringeSize.push_back(puzzleHistoryQueue.size());
+        if (isCorrectSolution(current.history)) {
+            endOfAlgorithm(type, &current, &fringeSize);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int newRow = current.RowLoc + dRow[i];
+            int newCol = current.ColLoc + dCol[i];
+
+            if (isValidRowAndCol(newRow, newCol)) {
+                PuzzleHistory nextCurrent = current;
+                std::swap(nextCurrent.history[current.RowLoc][current.ColLoc],
+                    nextCurrent.history[newRow][newCol]);
+
+                nextCurrent.RowLoc = newRow;
+                nextCurrent.ColLoc = newCol;
+                nextCurrent.path += moveString[i];
+
+                if (visitedSet.find(nextCurrent.former()) == visitedSet.end()) {
+                    puzzleHistoryQueue.push(nextCurrent);
+                    visitedSet.insert(nextCurrent.former());
+                }
+            }
+        }
+        ++depth;
+        if (++iterationCount % printInterval == 0) {
+            std::cout << "Stage: " << iterationCount << "\n";
+            printGrid(current.history);
+        }
+    }
+    std::cout << "No Solution Found via BFS. \n";
+    /*
+    std::vector<int> fringeSize;
+
+    for (int depth = 0; depth < depth_limit; ++depth) {
+        std::unordered_set<std::string> visitedSet;
+        PuzzleHistory firstState = { {}, row, col, "" };
+        std::memcpy(firstState.history, grid, sizeof(firstState.history));
+
+        if (DLS(firstState, 0, depth_limit, visitedSet)) {
+            std::cout << "Solution Found via IDDFS. \n";
+            return;
+        }
+    }
+    std::cout << "No Solution Found via IDDFS. \n";
+    */
+}
 
 int main() {
     short int arr[3][3] = { {1, 2, 4}, {0, 5, 7}, {8, 3, 6} };
@@ -236,17 +329,22 @@ int main() {
     time1->endTimer();
     */
 
+    /*
     Timer* time2 = new Timer();
     DFSL(arr, zeroRowLoc, zeroColLoc, 20);
     time2->endTimer();
+    */
 
+    Timer* time3 = new Timer();
+    IDDFS(arr, zeroRowLoc, zeroColLoc, 200);
+    time3->endTimer();
+    
     return 0;
 }
 
 bool isValidRowAndCol(int row, int col) {
     return (row >= 0 && row < 3 && col >= 0 && col < 3);
 }
-
 bool isCorrectSolution(short int grid[3][3]) {
     for (short int i = 0; i < 3; i++)
         for (short int j = 0; j < 3; j++)
@@ -254,7 +352,6 @@ bool isCorrectSolution(short int grid[3][3]) {
                 return false;
     return true;
 }
-
 void printGrid(short int grid[3][3]) {
     std::string str;
     for (int i = 0; i < 3; i++) {
@@ -269,7 +366,6 @@ void printGrid(short int grid[3][3]) {
     std::cout << "------------------" << std::endl;
     file.writeTheFile(str);
 }
-
 void endOfAlgorithm(SearchType type,PuzzleHistory* current, std::vector<int>* fringeSize) {
     switch (type)
     {
@@ -281,6 +377,9 @@ void endOfAlgorithm(SearchType type,PuzzleHistory* current, std::vector<int>* fr
         break;
     case 2:
         std::cout << "DFSL Solution Found:\n";
+        break;
+    case 3:
+        std::cout << "IDDFS Solution Found:\n";
         break;
     default:
         break;
